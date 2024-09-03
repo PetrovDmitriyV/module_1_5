733a4a6a37f5822420c599c4159f07b362518ac1
PetrovDmitriyV <phanl2@mail.ru>
2024-09-03 19:36:39 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = (1, 2, True, "Яблоко", "Слива")
+print(immutable_var)
+# immutable_var.append("Груша")
+print("Нельзя добавить элемент в кортеж")
+module_list = [1, 2, True, "Яблоко", "Слива"]
+print(module_list)
+module_list.append("Груша")
+print(module_list)
